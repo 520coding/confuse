71a8dc55b91bd5688895baccc1bf038c8ca37889
YJS <1016281864@qq.com>
2020-12-09 18:22:01 +0800

    v2.8.0（2020.12.09） 1. 重构[修改工程] a. 移除bundleID、Version混淆，原因：该部分改动简单且主要由用户自定义 b. 新增‘修改UUID’，所有xx.xcodeproj的uuid都将被修改，如需过滤，请在黑名单中设置 c. 新增‘修改target’，所有自定义target名称，及相关联的内容、文件名等都将被修改 2. 优化xib、storyboard修改时有可能改变视图层级关系导致视图被盖住 3. 新增‘敏感词’自定义 4. 移除[重命名多语言][插入文件][插入文本]，原因：该三个功能与其他工具类似，个人认为该方式已过时，且与新版[修改工程]冲突，故移除，后续将重构 5. 移除项目基本配置的‘其他target名称’设置 6. 优化代码，减少混淆前期时间25秒左右

---
+++ confuse_test/confuse_sdk/confuse_sdk.h
@@ -0,0 +1,19 @@
+//
+//  confuse_sdk.h
+//  confuse_sdk
+//
+//  Created by yjs on 2020/12/8.
+//  Copyright © 2020 coding520. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for confuse_sdk.
+FOUNDATION_EXPORT double confuse_sdkVersionNumber;
+
+//! Project version string for confuse_sdk.
+FOUNDATION_EXPORT const unsigned char confuse_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <confuse_sdk/PublicHeader.h>
+
+
