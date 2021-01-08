28db4159e37a2b8dba4de949fb0bd9d0361e1b3b
Takemoto-xie <1085192695@qq.com>
2021-01-08 17:36:09 +0800

    v3.5.0（2021.01.08） 1. 新增OC[重命名多语言]，对直接或间接使用系统方法NSLocalizedString、NSLocalizedStringFromTable的多语言进行修改 2. 优化OC[重命名图片]、[插入文件]、[插入文本]，减少混淆时间20%~30%，文件不包含字符串时不处理 3. 修复OC[插入文件]bug，插入文件概率性出错（出现同名局部变量名view，导致编译报错）

---
+++ confuse_test/confuse_test/oc/localizable/TestLocalizableViewController.h
@@ -0,0 +1,17 @@
+//
+//  TestLocalizableViewController.h
+//  confuse_test
+//
+//  Created by ymac on 29/12/2020.
+//  Copyright © 2020 coding520. All rights reserved.
+//
+
+#import "ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestLocalizableViewController : ViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
