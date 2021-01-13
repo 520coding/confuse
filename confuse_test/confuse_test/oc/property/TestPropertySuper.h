14061ff20f361dcbb175f4e13bcbbe1304fa1a60
YJS <1016281864@qq.com>
2021-01-13 20:05:32 +0800

    v3.6.0（2021.01.13） 1. 新增C++[修改方法]，利用重载技术修改函数原型并调用修改形参 2. 新增部分进度提示信息，避免大项目卡顿假象 3. 优化OC[重命名图片]，大项目中预估再减少混淆时间20%~30% 4. 优化OC[修改图片]，升级为通用功能 5. 优化通用功能，使其完全独立，减少混淆时间，目前可当成小工具使用，基本适用所有iOS项目 6. 修复上一个版本呢‘Scheme’名称无法自动识别

--- confuse_test/confuse_test/oc/property/TestPropertySuper.h
+++ confuse_test/confuse_test/oc/property/TestPropertySuper.h
@@ -14,7 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @protocol TestPropertySuper <NSObject, Base>
 
-@property (nonatomic, strong) NSString *sp2;//测试 1.继承、多个类型继承是否统一
+@property (nonatomic, strong) NSString *sp2;
 @property (nonatomic, strong) NSString *sp3;
 
 @end
@@ -23,7 +23,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic, strong) NSObject *sp1
         ;
-@property (nonatomic, strong) NSObject *more;//测试 多子类继承是否统一
+@property (nonatomic, strong) NSObject *more;
 @property (nonatomic, strong) NSString *name;
 @property (nonatomic, strong) NSString *Age;
 @property (nonatomic, strong) NSString *NICK;
