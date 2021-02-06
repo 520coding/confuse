35105d14585cc339abd5c72788c7d5bf3d170064
Takemoto-xie <1085192695@qq.com>
2021-02-06 17:45:36 +0800

    v3.9.3（2021.02.06） 1. 优化OC[插入文件] a. 插入的文件互相调用 b. 提高文件在项目中的调用频率，之前仅在controller的viewDidLoad方法中才会调用 c. 移除旧版本混淆模式，原因：该模式与版本迭代混淆不兼容 2. 优化OC[插入文本]，提高文本在项目中的调用频率 3. 优化[版本迭代混淆]，项目中90%功能均支持更新混淆

---
+++ confuse_test/confuse_test/oc/template/TemplateLastClassModel.m
@@ -0,0 +1,13 @@
+//
+//  TemplateLastClassModel.m
+//  confuse_test
+//
+//  Created by ymac on 28/01/2021.
+//  Copyright © 2021 coding520. All rights reserved.
+//
+
+#import "TemplateLastClassModel.h"
+
+@implementation TemplateLastClassModel
+
+@end
---
+++ confuse_test/confuse_test/oc/template/TemplateLastClassView.h
@@ -0,0 +1,20 @@
+//
+//  TemplateLastClassView.h
+//  confuse_test
+//
+//  Created by ymac on 28/01/2021.
+//  Copyright © 2021 coding520. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TemplateLastClassModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TemplateLastClassView : UIView
+
+- (void)setTemplateLastClassModel:(TemplateLastClassModel *)model;
+
+@end
+
+NS_ASSUME_NONNULL_END
