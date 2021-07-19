dcea15466698f44f4ae41698ed243a9b3282eaac
YJS <1016281864@qq.com>
2021-07-20 00:56:57 +0800

    v4.6.0 (2021.07.21) 1.Added Debug mode, you can view the comparison before and after the modification in the source file to avoid the difficulty of git comparison after the file name modification or major changes 2.Added Swift [Rename Picture], smart noun replacement, and automatically correct the situation where the picture name and the xcassets folder name do not correspond to each other a.You can set a ‘run splicing name’ for the image name generated by string splicing at runtime b.‘Ignore dangerous names’ switch can be set

---
+++ confuse_test/confuse_test/swift/source/image/SwiftImageModel.swift
@@ -0,0 +1,14 @@
+//
+//  SwiftImageModel.swift
+//  confuse_test
+//
+//  Created by yjs on 2021/6/21.
+//  Copyright © 2021 coding520. All rights reserved.
+//
+
+import Foundation
+
+class SwiftImageModel {
+    var title = ""
+    var image = ""
+}
